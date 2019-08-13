97a070ff268fc5ffcb61c84111c1915538b97996
Nic <thisisnic@gmail.com>
2019-08-13 20:47:32 +0100

    Add build & deploy scripts for Travis

--- _deploy.sh
+++ _deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-set -e
+# configure your name and email if you have not done so
+git config --global user.email "thisisnic@gmail.com"
+git config --global user.name "Nic"
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
-
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
 cd book-output
+git rm -rf *
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m"Update the book"
 git push -q origin gh-pages
